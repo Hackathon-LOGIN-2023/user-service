44e5a91e6f0a0fb671281b71284b72e9f4a8e168
Jhonatan Cagua <jhonatan-mcniven.cagua-herrera@imt-atlantique.net>
2023-01-12 20:34:19 +0100

    Update URI MongoDB

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 //const MONGODB_URI = process.env.MONGODB_URI;
 const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE } = process.env;
-const MONGODB_URI = `mongodb://${'127.0.0.1'}/${'notes-app'}`;
+const MONGODB_URI = `mongodb://user-services-8167:jWKhNHwZRYXacpCe715z@c4a56388-b2ef-489f-be32-c79ba86eabc0.user-services-8167.mongo.a.osc-fr1.scalingo-dbs.com:30476/user-services-8167?replicaSet=user-services-8167-rs0&ssl=true`;
 
 console.log("url mongo ",MONGODB_URI);
 
